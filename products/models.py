43b440ac528e72642e7e3afdacfbf32889069606
GurraT <gustafthomsson@hotmail.com>
2021-06-14 19:25:14 +0000

    added product detail functionality

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.TextField(max_length=138)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     img_url = models.URLField(max_length=1024, null=True, blank=True)
