83ec52c90da376899bc1d5994b8cbd90465f8ff8
GurraT <gustafthomsson@hotmail.com>
2021-06-25 04:10:43 +0000

    setting up stripe, adjusting checkout form, added card row, added css and js to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -10,12 +14,18 @@ def checkout(request):
         messages.warning(request, 'Your bag is empty at the moment')
         return redirect(reverse('products'))
     
+    current_bag =bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+
     order = Order.objects.all()
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'order': order
+        'order': order,
+        'stripe_public_key' : 'pk_test_51IqicmJkdyXzLxmX6JMO0yvSUBSoBQQH55IEhBkFS2t29G8H8DyLvQqVsT4ExZ8BuQiVvJd78EBZ46RSrZjFhOXI005WNVmLp2',
+        'client_secret' : 'test client secret',
     }
     
     return render(request, template, context)
