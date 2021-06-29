1d65c687ff93bb990143f30e1541fb339b089afa
GurraT <gustafthomsson@hotmail.com>
2021-06-29 13:20:17 +0000

    working with profile app, need to update webhook

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 
+from django.contrib import messages
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
-# Create your views here.
+from checkout.models import Order
 
 
 def profile(request):
