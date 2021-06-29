053c077708d1fca25536ac978873d5cdee353c12
GurraT <gustafthomsson@hotmail.com>
2021-06-29 04:58:18 +0000

    added order_history to profiles view

--- profiles/views.py
+++ profiles/views.py
@@ -28,3 +28,19 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request,(
+        f'This is your earlier filed orders {{order_number}}.'
+        'Confirmation email was sent to you on the order date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
+    }
+
+    return render(request, template, context)
