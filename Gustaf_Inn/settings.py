f4651fbdc0122cb124af4daaa370619997f6aa16
GurraT <gustafthomsson@hotmail.com>
2021-07-09 10:09:15 +0000

    sat up email connection

--- Gustaf_Inn/settings.py
+++ Gustaf_Inn/settings.py
@@ -183,22 +183,22 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-   AWS_STORAGE_BUCKET_NAME = 'gustaf-inn'
-   AWS_S3_REGION_NAME = 'eu-north-1'
-   AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
-   AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
-   AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'  
+    AWS_STORAGE_BUCKET_NAME = 'gustaf-inn'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'  
 
-    #STATIC AND MEDIA FILES
-   STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-   STATICFILES_LOCATION = 'static'
-   DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-   MEDIAFILES_LOCATION = 'media'
+        #STATIC AND MEDIA FILES
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
 
-    #override static and media files in production
+        #override static and media files in production
 
-   STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-   MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
