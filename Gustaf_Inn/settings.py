6431c52c1ad0d1ff47367c177397567e194ba8f9
GurraT <gustafthomsson@hotmail.com>
2021-07-09 06:52:57 +0000

    troubleshooting aws setup

--- Gustaf_Inn/settings.py
+++ Gustaf_Inn/settings.py
@@ -180,7 +180,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
    AWS_STORAGE_BUCKET_NAME = 'gustaf-inn-static-files-user'
-   AWS_S3_REGION_NAME = 'sts.eu-north-1'
+   AWS_S3_REGION_NAME = 'eu-north-1'
    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'  
